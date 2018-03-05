2cb960e9c3ea0ac13dc13b1c0d69d93d87e17bec
fangnan124 <fangnan124@gmail.com>
2018-03-05 12:42:34 +0900

    fix issue resuce Octokit::AbuseDetected

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  # class Api::Github::NonExistentRepositoryError < StandardError; end
-  #
-  # rescue_from Api::Github::NonExistentRepositoryError do |exception|
-  #   flash[:notice] = "Repository does not exist."
-  # end
+  rescue_from Octokit::AbuseDetected do |exception|
+    flash[:notice] = "Please wait a few minutes before you try again."
+    render 'files_explorer/index'
+  end
 end
